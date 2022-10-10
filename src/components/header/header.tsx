d60aeda7f9669b4ab88524403df2c3d0c259e48c
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-10-10 12:13:13 +0200

    Logged in user photo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,21 +1,44 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, useContext } from '@builder.io/qwik'
 import { Link } from '@builder.io/qwik-city'
+import { AppContext } from '~/root'
 import { signUserInGoogle } from './signUserInGoogle'
 
 export default component$(() => {
+  const userState = useContext(AppContext)
+
   return (
     <header class="bg-primary px-5 flex fixed w-full h-top-menu flex items-center">
       <Link class="flex-1 text-white text-xl" href="/">
         Ages & Birthdays
       </Link>
-      <button
-        type="button"
-        onClick$={() => {
-          signUserInGoogle()
-        }}
-      >
-        <img src="/icon-smile.svg" class="w-[25px]" />
-      </button>
+      {userState.user ? (
+        <button
+          type="button"
+          class="flex border border-white rounded-full"
+          style="width: 36px; height: 36px"
+        >
+          {userState.user.photoUrl ? (
+            <img
+              src={userState.user.photoUrl}
+              alt={userState.user.name}
+              class="rounded-full"
+            />
+          ) : (
+            <div className="flex w-full h-full items-center justify-center text-xl">
+              {userState.user.name.substring(0, 1)}
+            </div>
+          )}
+        </button>
+      ) : (
+        <button
+          type="button"
+          onClick$={() => {
+            signUserInGoogle()
+          }}
+        >
+          <img src="/icon-smile.svg" className="w-[25px]" />
+        </button>
+      )}
     </header>
   )
 })
