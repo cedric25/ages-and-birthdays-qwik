9a0f185086d73197f0d3595d770e32845bba3d02
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-10-12 14:45:12 +0200

    Little green dot if active filters

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -1,24 +1,38 @@
-import { $, component$, mutable, useContext, useStore } from '@builder.io/qwik'
+import {
+  $,
+  component$,
+  mutable,
+  useContext,
+  useStore,
+  useWatch$,
+} from '@builder.io/qwik'
 import { AppContext, UserState } from '~/root'
 import { Chip } from '~/components/chip/chip'
 import Search from '~/components/filters/search'
-
-type State = {
-  showFilters: boolean
-}
 
 export function isGroupSelected(groupLabel: string, userState: UserState) {
   return userState.selectedGroups.indexOf(groupLabel) !== -1
 }
 
 export default component$(() => {
-  const state = useStore<State>({
+  const state = useStore<{
+    showFilters: boolean
+    hasActiveFilters: boolean
+  }>({
     showFilters: false,
     // FOR TESTS
     // showFilters: true,
+    hasActiveFilters: false,
   })
 
   const userState = useContext(AppContext)
+
+  useWatch$(({ track }) => {
+    track(userState, 'selectedGroups')
+    track(userState, 'searchTerm')
+    state.hasActiveFilters =
+      !!userState.selectedGroups.length || !!userState.searchTerm
+  })
 
   const expandAnimationProps = {
     duration: 300,
@@ -69,12 +83,17 @@ export default component$(() => {
     <>
       <div class="m-2 mb-0 text-center">
         <button
-          class={`h-12 rounded-lg px-3 tracking-wide h-10 transition-all ${
+          class={`relative h-12 rounded-lg px-3 tracking-wide h-10 transition-all ${
             state.showFilters ? 'bg-gray-200 w-[72px]' : 'bg-sky-200 w-full'
           }`}
           onClick$={toggleShowFilters}
         >
           {state.showFilters ? 'Close' : 'Filters'}
+          {!state.showFilters && state.hasActiveFilters && (
+            <span class="absolute top-[10px] ml-[4px] w-2 h-2 rounded-full bg-green-500">
+              &nbsp;
+            </span>
+          )}
         </button>
       </div>
       <div
