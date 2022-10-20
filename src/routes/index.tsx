681dce1f9792e01d63c20ed0db4e7fe7fce0ea8d
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-10-20 22:02:32 +0200

    Don't show filters button if nobody in list

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,18 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, useContext } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
+import { AppContext } from '~/appContext'
 import List from '~/components/list/list'
 import Filters from '~/components/filters/filters'
 
 export default component$(() => {
+  const userState = useContext(AppContext)
+
+  const hasAtLeastOnePerson =
+    Object.values(userState.importantPersons).length > 0
+
   return (
     <>
-      <Filters />
+      {hasAtLeastOnePerson && <Filters />}
       <div class="mb-24 py-3 px-5">
         <List />
       </div>
